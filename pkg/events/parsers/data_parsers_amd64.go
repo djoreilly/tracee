8a1f2b2a3c78072ee99c0918428a3c58c875226f
Jeff Mahoney <jeffm@jeffm.io>
2025-03-14 16:49:42 +0000

    parsers: move O_DIRECT, O_DIRECTORY, and O_NOFOLLOW to arch-specific code
    O_LARGEFILE isn't the only flag that varies by architecture.  O_DIRECT,
    O_DIRECTORY, and O_NOFOLLOW do as well.

--- pkg/events/parsers/data_parsers_amd64.go
+++ pkg/events/parsers/data_parsers_amd64.go
@@ -10,7 +10,10 @@ var (
 	// from asm-generic/fcntl.h
 	// NOT sequential values
 	// gap
+	O_DIRECT    = SystemFunctionArgument{rawValue: 00040000, stringValue: "O_DIRECT"}
 	O_LARGEFILE = SystemFunctionArgument{rawValue: 00100000, stringValue: "O_LARGEFILE"}
+	O_DIRECTORY = SystemFunctionArgument{rawValue: 00200000, stringValue: "O_DIRECTORY"}
+	O_NOFOLLOW  = SystemFunctionArgument{rawValue: 00400000, stringValue: "O_NOFOLLOW"}
 )
 
 var openFlagsValues = []SystemFunctionArgument{
--- pkg/events/parsers/data_parsers_arm64.go
+++ pkg/events/parsers/data_parsers_arm64.go
@@ -6,7 +6,10 @@ var (
 	// from asm/fcntl.h
 	// NOT sequential values
 	// gap
-	O_LARGEFILE = SystemFunctionArgument{rawValue: 0400000, stringValue: "O_LARGEFILE"}
+	O_DIRECTORY = SystemFunctionArgument{rawValue: 00040000, stringValue: "O_DIRECTORY"}
+	O_NOFOLLOW  = SystemFunctionArgument{rawValue: 00100000, stringValue: "O_NOFOLLOW"}
+	O_DIRECT    = SystemFunctionArgument{rawValue: 00200000, stringValue: "O_DIRECT"}
+	O_LARGEFILE = SystemFunctionArgument{rawValue: 00400000, stringValue: "O_LARGEFILE"}
 )
 
 var openFlagsValues = []SystemFunctionArgument{
